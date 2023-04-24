6c904d6065a6e07003798fa64eb516ec9bdaa41e
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-04-24 14:53:38 -0600

    Initial commit.

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about-me">About Me</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
