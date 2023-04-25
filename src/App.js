530c015d9ddf6975db07409746c0a90055784047
Jacob Hewitson <jacob.a.hewitson@gmail.com>
2023-04-24 20:01:32 -0600

    fixed styling for first paragraph

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+      <div className="heading">
+        <h1>Jacob Hewitson</h1>
+        <h2>Software Developer, Passionate Learner, Ideal Employee</h2>
+      </div>
       <Navbar />
-      <div id="home">
+      <div className="site-content" id="home">
+        <h3>Welcome!</h3>
+        <p>
+          My name is Jacob Hewitson. I am a Software Engineer with a B.S. of
+          Computer Science. I'm really excited to work with you and provide
+          quality products to meet your software needs. Here on my website you
+          can see different projects I've worked on or am currently working on!
+        </p>
+        <button>
+          <a href="#projects">Click here!</a>
+        </button>
+      </div>
+      <div className="site-content" id="about-me">
         <p></p>
       </div>
-      <div id="about-me">
+      <div className="site-content" id="projects">
         <p></p>
       </div>
-      <div id="projects">
-        <p></p>
-      </div>
-      <div id="contact">
+      <div className="site-content" id="contact">
         <p></p>
       </div>
     </div>
